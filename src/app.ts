afb2bb3cf39dc50badf548bf17704165a5942d40
Oleksiy Dubovyk <oleksiy.dubovyk@racingpost.com>
2018-02-28 17:17:37 +0200

    [task7] Defining an Interface

--- src/app.ts
+++ src/app.ts
@@ -6,28 +6,44 @@ function showHello(divName: string, name: string) {
 
 
 // ### Functions
-// # Task 06. Function Overloading
+// # Task 07. Defining an Interface
 enum Category { JavaScript, CSS, HTML, TypeScript, Angular2, Software }
 
-function getAllBooks(): any[] {
-    const books: any[] = [
+interface Book {
+    id: number;
+    title: string;
+    author: string;
+    available: boolean;
+    category: Category;
+    pages?: number;
+    markDamaged?: (reason: string) => void;
+}
+function getAllBooks(): Book[] {
+    const books: Book[] = [
         { id: 1, title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true, category: Category.JavaScript },
         { id: 2, title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false, category: Category.JavaScript  },
         { id: 3, title: 'CSS Secrets', author: 'Lea Verou', available: true, category: Category.CSS },
         { id: 4, title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true, category: Category.JavaScript  }
     ];
-
     return books;
 }
-
-function getTitles(author: string): string[];
-function getTitles(available: boolean): string[];
-function getTitles(property: string | boolean): string[] {
-    if (typeof property === 'string') {
-        return getAllBooks().filter(book => book.author === property).map(book => book.title);
-    } else if (typeof property === 'boolean') {
-        return getAllBooks().filter(book => book.available === property).map(book => book.title);
-    }
+function getBookById(id): Book | undefined {
+    const books = getAllBooks();
+    return books.find(book => book.id === id);
 }
-console.log(getTitles('Lea Verou'));    // books by Lea Verou
-console.log(getTitles(false));          // unavailable books
+function printBook(book: Book): void {
+    console.log(`${book.title} by ${book.author}`);
+}
+const myBook: Book = {
+    id: 5,
+    title: 'Colors, Backgrounds, and Gradients',
+    author: 'Eric A. Meyer',
+    available: true,
+    category: Category.CSS,
+    // year: 2015,
+    // copies: 3
+    pages: 200,
+    markDamaged: (reason: string) => console.log(`Damaged: ${reason}`),
+};
+printBook(myBook);
+myBook.markDamaged('missing back cover');
